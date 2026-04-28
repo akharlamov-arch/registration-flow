3eeccd00fef45ca31fb36c55d83a5691e01ce6ea
Andrei <akharlamov@itrucking.org>
2026-04-27 17:16:26 -0700

    Remove incorrect info warning from account review step

--- src/pages/OtpVerification.jsx
+++ src/pages/OtpVerification.jsx
@@ -2094,20 +2094,6 @@ export default function OtpVerification() {
             </p>
           </div>
 
-          <div className="p-4 bg-red-50 border border-red-200 rounded-xl flex items-start gap-3">
-            <svg className="w-5 h-5 text-red-500 flex-shrink-0 mt-0.5" fill="none" viewBox="0 0 24 24"
-                 stroke="currentColor" strokeWidth={1.75} aria-hidden="true">
-              <path strokeLinecap="round" strokeLinejoin="round"
-                    d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
-            </svg>
-            <p className="text-sm text-red-800 leading-relaxed">
-              {t('accountReview.errorNotice')}{' '}
-              <a href="tel:+19162694606" className="font-semibold whitespace-nowrap hover:underline">
-                (916) 269-4606
-              </a>
-            </p>
-          </div>
-
           {/* Marketing consent */}
           <label className="flex items-start gap-3 cursor-pointer group">
             <div className="mt-0.5 flex-shrink-0">
